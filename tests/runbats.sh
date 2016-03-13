8f278487ad700d496f1122b5547edfaef9ffd807
Bert Van Vreckem <bert.vanvreckem@gmail.com>
2016-03-13 20:38:20 +0100

    Improved test runner for BATS acceptance tests

--- tests/runbats.sh
+++ tests/runbats.sh
@@ -11,63 +11,96 @@
 set -o errexit  # abort on nonzero exitstatus
 set -o nounset  # abort on unbound variable
 
-#{{{ Variables
+#{{{ Constants
 
-test_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+readonly DEFAULT_INSTALL_DIR="/opt"
+readonly TEST_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+readonly TEST_FILE_PATTERN="*.bats"
 
-bats_archive="v0.4.0.tar.gz"
-bats_url="https://github.com/sstephenson/bats/archive/${bats_archive}"
-bats_install_dir="${test_dir}/bats"
-bats="${bats_install_dir}/libexec/bats"
-
-test_file_pattern="*.bats"
+readonly BATS_ARCHIVE="v0.4.0.tar.gz"
+readonly BATS_URL="https://github.com/sstephenson/bats/archive/${BATS_ARCHIVE}"
 
 # color definitions
-Blue='\e[0;34m'
-Yellow='\e[0;33m'
-Reset='\e[0m'
+readonly BLUE='\e[0;34m'
+readonly YELLOW='\e[0;33m'
+readonly RESET='\e[0m'
 
 #}}}
 #{{{ Functions
 
-# Usage: install_bats_if_needed
-install_bats_if_needed() {
-  pushd "${test_dir}" > /dev/null
-  if [[ ! -d "${bats_install_dir}" ]]; then
-    wget "${bats_url}"
-    tar xf "${bats_archive}"
-    mv bats-* bats
-    rm "${bats_archive}"
+# Usage: bats_install_dir
+# Determines where BATS should be installed. When run as root, install under
+# $DEFAULT_INSTALL_DIR, otherwise under the directory where the script is run
+# from.
+bats_install_dir() {
+  if [ "${UID}" -eq "0" ]; then
+    echo "${DEFAULT_INSTALL_DIR}"
+  else
+    echo "${TEST_DIR}"
   fi
-  popd > /dev/null
 }
 
-# find_tests DIR [MAX_DEPTH]
+# Usage: install_bats DIR
+# Installs BATS in the specified directory
+install_bats() {
+  local install_dir="${1}"
+  if [ ! -d "${install_dir}/bats" ]; then
+    pushd "${install_dir}" > /dev/null
+    wget "${BATS_URL}"
+    tar xf "${BATS_ARCHIVE}"
+    mv bats-* bats
+    rm "${BATS_ARCHIVE}"
+    popd > /dev/null
+  fi
+}
+
+# Usage: bats_executable
+# Returns the path to the BATS executable. If BATS is not available on the
+# system, it is installed.
+bats_executable() {
+  local bats=""
+  if ! which bats > /dev/null 2>&1; then
+    local dir
+    dir="$(bats_install_dir)"
+    install_bats "${dir}"
+    bats="${dir}/bats/libexec/bats"
+  else
+    bats="$(which bats)"
+  fi
+  echo "${bats}"
+}
+
+# Usage: find_tests DIR [MAX_DEPTH]
+# Finds BATS test files in the specified directory, up to the specified depth
+# (if specified)
 find_tests() {
   local max_depth=""
   if [ "$#" -eq "2" ]; then
     max_depth="-maxdepth $2"
   fi
 
-  local tests=$(find "$1" ${max_depth} -type f -name "${test_file_pattern}" -printf '%p\n' 2> /dev/null)
+  local tests
+  tests=$(find "$1" ${max_depth} -type f -name "${TEST_FILE_PATTERN}" -printf '%p\n' 2> /dev/null)
 
   echo "${tests}"
 }
 #}}}
 # Script proper
 
-install_bats_if_needed
+# Get the path to the BATS executable
+bats="$(bats_executable)"
 
 # List all test cases (i.e. files in the test dir matching the test file
 # pattern)
+
 # Tests to be run on all hosts
-global_tests=$(find_tests "${test_dir}" 1)
+global_tests=$(find_tests "${TEST_DIR}" 1)
 
 # Tests for individual hosts
-host_tests=$(find_tests "${test_dir}/${HOSTNAME}")
+host_tests=$(find_tests "${TEST_DIR}/${HOSTNAME}")
 
 # Loop over test files
 for test_case in ${global_tests} ${host_tests}; do
-  echo -e "${Blue}Running test ${Yellow}${test_case}${Reset}"
+  echo -e "${BLUE}Running test ${YELLOW}${test_case}${RESET}"
   ${bats} "${test_case}"
 done
